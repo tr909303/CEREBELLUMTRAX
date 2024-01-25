7cf6a0844035dba3f02df9f54bde4b773e12c649
cardaojr <cardao1997@gmail.com>
2024-01-25 01:44:02 +0000

    UPDATE

---
+++ public/player/defaultPlayList.js
@@ -0,0 +1,40 @@
+﻿/* This is your default Playlist which automatically load when the page loads */
+
+var defaultPlayList =  [
+			
+
+						{
+
+title:"Cardao - 90s Guetto",
+	 mp3:"promo/audio/1.Cardao - 90s Guetto (Original Mix).mp3"
+			},
+
+{
+
+title:"Cardao - Urban Mythos (Original Mix)",
+	 mp3:"promo/audio/2.Cardao - Urban Mythos (Original Mix).mp3"
+			},
+			
+			{
+
+title:"Cardao - Street Sonata (Original Mix)",
+	 mp3:"promo/audio/3.Cardao - Street Sonata (Original Mix).mp3"
+			},
+
+			{
+
+title:"Cardao - Ritual Guetto Old Rhythms (Original Mix)",
+	 mp3:"promo/audio/4.Cardao - Ritual Guetto Old Rhythms (Original Mix).mp3"
+			},
+
+		
+
+		
+
+
+				
+
+];
+		
+/* change this value to adjust if the song should start automatically or wait for user to press play */
+var autoPlay = false;
